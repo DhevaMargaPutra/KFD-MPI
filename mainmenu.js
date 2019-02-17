7be720d88e0d30b302a297e2c3ec913fc34379bb
DhevaMargaPutra <dhevamp57@gmail.com>
2019-02-17 14:10:49 +0700

    mainmenu-transition-04

--- mainmenu.js
+++ mainmenu.js
@@ -5,3 +5,23 @@ function showLabel(element) {
 function hideLabel(element) {
     element.children[0].style.display = 'none';
 }
+
+const transitionDOM = document.getElementById('transition');
+function goTo(menu) {
+    switch(menu) {
+        case 'materi':
+            transitionDOM.classList.add('materi-trans', 'animated', 'slideInRight');
+            break;
+        case 'evaluasi':
+            transitionDOM.classList.add('evaluasi-trans', 'animated', 'slideInRight');
+            break;
+        case 'simulasi':
+            transitionDOM.classList.add('simulasi-trans', 'animated', 'slideInRight');
+            break;
+        case 'tentang':
+            transitionDOM.classList.add('tentang-trans', 'animated', 'slideInRight');
+            break;
+        default:
+            console.error('no menu has selected!');
+    }
+}
