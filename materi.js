974f066baf8c755c0ef58eec66bd78794df55b3d
DhevaMargaPutra <dhevamp57@gmail.com>
2019-02-19 22:21:18 +0700

    materi-final-04

--- materi.js
+++ materi.js
@@ -45,7 +45,39 @@ langsung kepada orang lain.
 
 Untuk menghasilkan ukuran cahaya yang tepat untuk menghasilkan bayangan, digunakan bantuan
 alat ukur lightmeter. Setelah mendapat ukuran cahaya yang tepat, seorang fotografer bisa mengatur
-cahaya tersebut dengan mengatur ASA (ISO Speed), diafragma (aperture), dan penggunaan filter.`,
+cahaya tersebut dengan mengatur ASA (ISO Speed), diafragma (aperture), dan penggunaan filter.
+
+https://adhiemedia.wordpress.com/download/komposisi-foto-digital/`,
+            halaman: 3
+        }
+    ],
+    [
+        {},
+        {
+            bab: 'BAB 2: Mengenal Segitiga Exposure',
+            gambar: 'Images/shutter.jpg',
+            teks:`
+Shutter Speed
+
+Shutter Speed atau biasa disebut dengan kecepatan rana adalah nilai kecepatan terbukanya jendela kamera sehingga cahaya bisa masuk ke dalam sensor kamera. Shutter speed bisa diibaratkan dengan keran air, jika kita membuka keran air dengan lama maka air yang keluar banyak, sebaliknya jika kita membuka keran air dengan cepat maka air yang dikeluarkan tentunya sedikit. Kira-kira begitu logika sederhana dari shutter speed ini. Oia, Satuan shutter speed adalah detik (s). `,
+            halaman: 1
+        },
+        {
+            bab: '',
+            gambar: '',
+            teks:`
+Kalo kalian mau membekukan subjek yang bergerak maka gunakan shutter speed yang cepat misalnya 1/500 atau 1/800 tapi disesuaikan dengan kondisi cahaya. Nah kalo kalian mau membuat subjek yang bergerak tersebut menjadi blur maka gunakan shutter speed yang lambat misalnya 1 s (detik) atau 1/4 s (detik) tergantung kondisi cahaya ya. Karena komponen yang lainnya seperti aperture dan ISO juga harus seimbang.`,
+            halaman: 2
+        },
+        {
+            bab: '',
+            gambar: 'Images/apreture.jpg',
+            teks:`
+Aperture/ Diafragma
+
+Aperture atau biasa disebut juga dengan diafragma adalah bukaan komponen lensa. Aperture ini terbuat dari bilah-bilah lempengan logam yang ditengahnya bisa membuka dengan lebar dan sempit sehingga mempengaruhi intensitas cahaya yang masuk ke dalam lensa sampai ke sensor kamera. Dalam fotografi aperture ini dilambangkan dengan huruf "f" dengan satuannya diameter bilah-bilah logam tersebut.
+
+Contoh penulisannya seperti ini f/1.8 atau f/x dimana "x" aalah diameter bilah-bilah tadi. Kalo logika sederhananya adalah aperture bagaikan besarnya pipa yang mengalirkan air.`,
             halaman: 3
         }
     ]
@@ -65,33 +97,33 @@ fillPage(2, materis[1][2]);
 function nextPage() {
     //sampai halaman terakhir return
     if (currentPage === materis[currentChapter].length - 1) {
-        alert('Sudah mencapai halaman terakhir. Klik bagian atas halaman untuk kembali ke halaman sebelumnya');
+        alert('Sudah mencapai halaman terakhir. Klik bagian atas halaman kertas untuk kembali ke halaman sebelumnya');
         return;
     }
     
-    //halaman depan di turunin, tambah animasi, setelah animasi buang animasi, tetap di bawah dulu
+    //halaman belakang diisi materi berikutnya
+    //jika materi setelahnya tidak ada nggak usah di isi
+    //halaman depan di turunin, setelah animasi selesai buang animasi, tetap di bawah dulu
     //halaman depan yang di bawah diisi materi sesuai halaman
     //halaman depan dipindah lagi di tengah
-    //halaman belakang diisi materi setelahnya
-    //jika materi setelahnya tidak ada nggak usah di isi
+    if (currentPage <= materis[currentChapter].length - 2) {
+        fillPage(2, materis[currentChapter][currentPage + 1]);
+    }
     foregroundPaperDOM.classList.add('mySlideOutDown');
     foregroundPaperDOM.addEventListener('animationend', function() {
         foregroundPaperDOM.style.transform = 'translateY(150%)';
         fillPage(1, materis[currentChapter][currentPage]);
         foregroundPaperDOM.style.transform = 'translateY(0%)';
-        if (currentPage <= materis[currentChapter].length - 2) {
-            fillPage(2, materis[currentChapter][currentPage + 1]);
-        }
         foregroundPaperDOM.classList.remove('mySlideOutDown');
     });
 
     currentPage++;
-    console.log(currentPage);
+    console.log('currentPage: ' + currentPage);
 }
 
 function prevPage() {
     if (currentPage === 1) {
-        alert('Halaman pertama. Klik bagian bawah halaman untuk beralih ke halaman selanjutnya');
+        alert('Halaman pertama. Klik bagian bawah halaman kertas untuk beralih ke halaman selanjutnya');
         return;
     }
 
@@ -106,7 +138,7 @@ function prevPage() {
     });
 
     currentPage--;
-    console.log(currentPage);
+    console.log('currentPage: ' + currentPage);
 }
 
 function fillPage(layer, materi) {
@@ -120,5 +152,62 @@ function fillPage(layer, materi) {
         backgroundPaperDOM.children[1].src = materi.gambar;
         backgroundPaperDOM.children[2].innerHTML = materi.teks;
         backgroundPaperDOM.children[3].innerHTML = materi.halaman;
+    }
+}
+
+function showLabel(element, navigation) {
+    element.children[0].style.display = 'initial';
+}
+
+function hideLabel(element) {
+    element.children[0].style.display = 'none';
+}
+
+function changeChapter(navigation) {
+    if(navigation === 'next') {
+        //jika currentChapter = 2, gak usah next, karena length 3
+        if (currentChapter === materis.length - 1) {
+            alert('Sudah mencapai Bab terakhir.\nKlik kertas yang ada di kiri layar untuk kembali ke bab sebelumnya!');
+            return;
+        }
+
+        //halaman belakang diisi currentPage
+        //pindah bab, currentChapter + 1, currentHalaman = 1
+        //halaman depan di seret dari kanan, diisi bab berikutnya halaman 1
+        fillPage(2, materis[currentChapter][currentPage]);
+
+        currentChapter++;
+        currentPage = 1;
+        console.log('currentChapter: ' + currentChapter);
+
+        fillPage(1, materis[currentChapter][currentPage]);
+
+        foregroundPaperDOM.classList.add('animated', 'slideInRight');
+        foregroundPaperDOM.addEventListener('animationend', function() {
+            foregroundPaperDOM.classList.remove('animated', 'slideInRight');
+        });
+    } else if(navigation === 'prev') {
+        //jika currentChapter = 1, gak usah prev, karena masih awal
+        if (currentChapter === 1) {
+            alert('Masih di awal Bab.\nKlik kertas yang ada di kanan layar untuk menuju ke bab berikutnya!');
+            return;
+        }
+
+        //halaman belakang diisi currentPage
+        //pindah bab, currentChapter - 1, currentHalaman = 1
+        //halaman depan diisi bab sebelumnya halaman 1
+        //halaman depan diseret dari kanan
+        fillPage(2, materis[currentChapter][currentPage]);
+
+        currentChapter--;
+        currentPage = 1;
+        console.log('currentChapter: ' + currentChapter);
+
+        fillPage(1, materis[currentChapter][currentPage]);
+
+        foregroundPaperDOM.classList.add('animated', 'slideInLeft');
+        foregroundPaperDOM.addEventListener('animationend', function() {
+            foregroundPaperDOM.classList.remove('animationend', 'slideInLeft');
+        });
     }
 }
