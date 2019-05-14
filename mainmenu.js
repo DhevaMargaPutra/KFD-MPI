294735e03cfb4a80a7832d5d426666c9abbe895d
dhevamargaputra <dhevamp57@gmail.com>
2019-05-14 16:09:56 +0700

    fix link error 1

--- mainmenu.js
+++ mainmenu.js
@@ -1,39 +1,39 @@
 function showLabel(element) {
-    element.children[0].style.display = 'initial';
+  element.children[0].style.display = "initial";
 }
 
 function hideLabel(element) {
-    element.children[0].style.display = 'none';
+  element.children[0].style.display = "none";
 }
 
-const transitionDOM = document.getElementById('transition');
+const transitionDOM = document.getElementById("transition");
 function goTo(menu) {
-    switch(menu) {
-        case 'materi':
-            transitionDOM.classList.add('materi-trans', 'animated', 'slideInRight');
-            transitionDOM.addEventListener('animationend', function() {
-                document.location.href = 'materi.html';
-            });
-            break;
-        case 'evaluasi':
-            transitionDOM.classList.add('evaluasi-trans', 'animated', 'slideInRight');
-            transitionDOM.addEventListener('animationend', function() {
-                document.location.href = 'evaluasi.html';
-            });
-            break;
-        case 'simulasi':
-            transitionDOM.classList.add('simulasi-trans', 'animated', 'slideInRight');
-            transitionDOM.addEventListener('animationend', function() {
-                document.location.href = 'simulasi.html';
-            });
-            break;
-        case 'tentang':
-            transitionDOM.classList.add('tentang-trans', 'animated', 'slideInRight');
-            transitionDOM.addEventListener('animationend', function() {
-                document.location.href = 'simulasi.html';
-            });
-            break;
-        default:
-            console.error('no menu has selected!');
-    }
+  switch (menu) {
+    case "materi":
+      transitionDOM.classList.add("materi-trans", "animated", "slideInRight");
+      transitionDOM.addEventListener("animationend", function() {
+        document.location.href = "./materi.html";
+      });
+      break;
+    case "evaluasi":
+      transitionDOM.classList.add("evaluasi-trans", "animated", "slideInRight");
+      transitionDOM.addEventListener("animationend", function() {
+        document.location.href = "evaluasi.html";
+      });
+      break;
+    case "simulasi":
+      transitionDOM.classList.add("simulasi-trans", "animated", "slideInRight");
+      transitionDOM.addEventListener("animationend", function() {
+        document.location.href = "simulasi.html";
+      });
+      break;
+    case "tentang":
+      transitionDOM.classList.add("tentang-trans", "animated", "slideInRight");
+      transitionDOM.addEventListener("animationend", function() {
+        document.location.href = "simulasi.html";
+      });
+      break;
+    default:
+      console.error("no menu has selected!");
+  }
 }
